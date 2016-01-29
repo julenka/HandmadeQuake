a7a6a48f7f18a967c4839ea12ef1889e1e8cc477
Philip Buuck <philipbuuck@gmail.com>
2016-01-28 20:05:27 -0500

    Handmade Quake 2.4
    More timing

--- Module2/main.c
+++ Module2/main.c
@@ -1,7 +1,55 @@
 #include <stdio.h>
 #include <windows.h>
 
-BOOL IsRunning = TRUE;
+static BOOL IsRunning = TRUE;
+
+//////////////////////////////
+//      TIMER CODE
+//////////////////////////////
+
+static double GTimePassed = 0;
+static double SecondsPerTick = 0;
+static __int64 GTimeCount = 0;
+
+float Sys_InitFloatTime(void)
+{
+    LARGE_INTEGER Frequency;
+    QueryPerformanceFrequency(&Frequency);
+
+    SecondsPerTick = 1.0 / (double)Frequency.QuadPart;
+
+    LARGE_INTEGER Counter;
+    QueryPerformanceCounter(&Counter);
+
+    GTimeCount = Counter.QuadPart;
+
+    return 0;
+}
+
+float Sys_FloatTime(void)
+{
+    LARGE_INTEGER Counter;
+    QueryPerformanceCounter(&Counter);
+
+    __int64 Interval = Counter.QuadPart - GTimeCount;
+    GTimeCount = Counter.QuadPart;
+
+    double SecondsGoneBy = (double)Interval * SecondsPerTick;
+    GTimePassed += SecondsGoneBy;
+
+    return (float)GTimePassed;
+}
+
+//////////////////////////////
+//      END TIMER CODE
+//////////////////////////////
+
+
+
+void Sys_Shutdown(void)
+{
+    IsRunning = FALSE;
+}
 
 LRESULT CALLBACK MainWndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
@@ -10,12 +58,14 @@ LRESULT CALLBACK MainWndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
     // catch any relevant messages here
     switch (uMsg)
     {
-    case WM_KEYUP:
-        IsRunning = FALSE;
-        break;
     case WM_ACTIVATE:
+        break;
+
     case WM_CREATE:
+        break;
+
     case WM_DESTROY:
+        Sys_Shutdown();
         break;
     default:
         Result = DefWindowProc(hWnd, uMsg, wParam, lParam);
@@ -48,10 +98,10 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     mainwindow = CreateWindowEx(
         0,
         "Module 2",
-        "Lesson 2.3",
+        "Lesson 2.4",
         WindowStyle,
-        200,
-        200,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
         r.right - r.left,
         r.bottom - r.top,
         NULL,
@@ -65,40 +115,28 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     PatBlt(DeviceContext, 0, 0, 800, 600, BLACKNESS);
     ReleaseDC(mainwindow, DeviceContext);
 
-    LARGE_INTEGER Frequency;
-    QueryPerformanceFrequency(&Frequency);
-
-    double SecondsPerTick = 1.0 / (double)Frequency.QuadPart;
-
-    LARGE_INTEGER Tick, Tock;
-    QueryPerformanceCounter(&Tick);
+    
+    float timecount = Sys_InitFloatTime();
 
     MSG msg;
     while (IsRunning)
     {
         // Check in with OS
-        if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+        while (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
         {
             TranslateMessage(&msg);
             DispatchMessage(&msg);
         }
     
-        // Update our game if it's time to
-        // Draw graphics if it's time to
 
-        Sleep(1000);
+        float newtime = Sys_FloatTime();
 
-        QueryPerformanceCounter(&Tock);
-
-        __int64 Interval = Tock.QuadPart - Tick.QuadPart;
-
-        double SecondsGoneBy = (double)Interval * SecondsPerTick;
 
         char buf[64];
-        sprintf_s(buf, 64, "Total time: %3.7f \n", SecondsGoneBy);
+        sprintf_s(buf, 64, "Total time: %3.7f \n", newtime);
         OutputDebugString(buf);
 
-        QueryPerformanceCounter(&Tick);
+        Sleep(1);
     }
 
     return 0;
