49d7d0a2ed47c77038391186c8a487570b06a9c3
Philip Buuck <philipbuuck@gmail.com>
2016-03-10 00:01:17 -0500

    Handmade Quake 4.3
    Loading Quake's Pak Files

--- Module4/Source.c
+++ Module4/Source.c
@@ -1,8 +1,38 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "file.h"
+
+typedef struct
+{
+    char Magic[4];
+    int  DirectoryOffset;
+    int  DirectoryLength;
+} dpackheader_t;
+
+typedef struct  
+{
+    char FileName[56];
+    int  FilePosition;
+    int  FileLength;
+} dpackfile_t;
+
+typedef struct  
+{
+    char FileName[64];
+    int  FilePosition;
+    int  FileLength;
+} packfile_t;
+
+typedef struct  
+{
+    char PackName[128];
+    int  PackHandle;
+    int  NumberOfFiles;
+    packfile_t *PakFiles;
+} pack_t;
 
 char* va(char* format, ...)
 {
@@ -18,34 +48,128 @@ char* va(char* format, ...)
     return b;
 }
 
+#define MAX_FILES_IN_PACK 2048
+
+pack_t *COM_LoadPackFile(char *PakFile)
+{
+    int PakSize;
+    int PakHandle = Sys_FileOpenRead(PakFile, &PakSize);
+
+    if (PakHandle == -1)
+    {
+        int a = 5; a;
+    }
+
+    dpackheader_t PakHeader;
+
+    Sys_FileRead(PakHandle, &PakHeader, sizeof(PakHeader));
+
+    if (PakHeader.Magic[0] != 'P' || PakHeader.Magic[1] != 'A' || PakHeader.Magic[2] != 'C' || PakHeader.Magic[3] != 'K')
+    {
+        int a = 5; a;
+    }
+
+    int NumPackFiles = PakHeader.DirectoryLength / sizeof(dpackfile_t);
+
+    packfile_t* FilesInMemory = malloc(NumPackFiles * sizeof(packfile_t));
+
+    Sys_FileSeek(PakHandle, PakHeader.DirectoryOffset);
+
+    dpackfile_t TempPackFile[MAX_FILES_IN_PACK];
+    Sys_FileRead(PakHandle, (void *)TempPackFile, PakHeader.DirectoryLength);
+
+    for (int i = 0; i < NumPackFiles; i++)
+    {
+        strcpy_s(FilesInMemory[i].FileName, 56, TempPackFile[i].FileName);
+        FilesInMemory[i].FileLength = TempPackFile[i].FileLength;
+        FilesInMemory[i].FilePosition = TempPackFile[i].FilePosition;
+    }
+
+    pack_t  *Pak0 = (void*)malloc(sizeof(pack_t));
+    strcpy_s(Pak0->PackName, 64, PakFile);
+    Pak0->PackHandle = PakHandle;
+    Pak0->NumberOfFiles = NumPackFiles;
+    Pak0->PakFiles = FilesInMemory;
+
+
+    return Pak0;
+}
+
 int main()
 {
-    int Size;
-    int FileHandle = Sys_FileOpenRead("Source.c", &Size);
+    pack_t *Pak0 = COM_LoadPackFile("id1\\pak0.pak");
+    pack_t *Pak1 = COM_LoadPackFile("id1\\pak1.pak");
 
-    if (FileHandle == -1)
-    {
-        int a = 5; a;
-    }
+    int a = 5; a;
 
-    void *Buffer = malloc(Size);
 
-    Sys_FileRead(FileHandle, Buffer, Size);
+    //int PakSize;
+    //int PakHandle = Sys_FileOpenRead("id1\\pak0.pak", &PakSize);
+    //
+    //if (PakHandle == -1)
+    //{
+    //    int a = 5; a;
+    //}
+    //
+    //dpackheader_t PakHeader;
+    //
+    //Sys_FileRead(PakHandle, &PakHeader, sizeof(PakHeader));
+    //
+    //if (PakHeader.Magic[0] != 'P' || PakHeader.Magic[1] != 'A' || PakHeader.Magic[2] != 'C' || PakHeader.Magic[3] != 'K')
+    //{
+    //    int a = 5; a;
+    //}
+    //
+    //int NumPackFiles = PakHeader.DirectoryLength / sizeof(dpackfile_t);
+    //
+    //packfile_t* FilesInMemory = malloc(NumPackFiles * sizeof(packfile_t));
+    //
+    //Sys_FileSeek(PakHandle, PakHeader.DirectoryOffset);
+    //
+    //dpackfile_t TempPackFile[MAX_FILES_IN_PACK];
+    //Sys_FileRead(PakHandle, (void *)TempPackFile, PakHeader.DirectoryLength);
+    //
+    //for (int i = 0; i < NumPackFiles; i++)
+    //{
+    //    strcpy_s(FilesInMemory[i].FileName, 56, TempPackFile[i].FileName);
+    //    FilesInMemory[i].FileLength = TempPackFile[i].FileLength;
+    //    FilesInMemory[i].FilePosition = TempPackFile[i].FilePosition;
+    //}
+    //
+    //pack_t  *Pak0 = (void*)malloc(sizeof(pack_t));
+    //strcpy_s(Pak0->PackName, 64, "id1\\pak0.pak");
+    //Pak0->PackHandle = PakHandle;
+    //Pak0->NumberOfFiles = NumPackFiles;
+    //Pak0->PakFiles = FilesInMemory;
 
-    int WriteFile = Sys_FileOpenWrite("Source.out");
 
-    if (WriteFile == -1)
-    {
-        int a = 5; a;
-    }
-    
-    Sys_FileWrite(WriteFile, Buffer, Size);
 
-    Sys_FileClose(FileHandle);
-    Sys_FileClose(WriteFile);
-
-    free(Buffer);
-    Buffer = NULL;
+    //int Size;
+    //int FileHandle = Sys_FileOpenRead("Source.c", &Size);
+    //
+    //if (FileHandle == -1)
+    //{
+    //    int a = 5; a;
+    //}
+    //
+    //void *Buffer = malloc(Size);
+    //
+    //Sys_FileRead(FileHandle, Buffer, Size);
+    //
+    //int WriteFile = Sys_FileOpenWrite("Source.out");
+    //
+    //if (WriteFile == -1)
+    //{
+    //    int a = 5; a;
+    //}
+    //
+    //Sys_FileWrite(WriteFile, Buffer, Size);
+    //
+    //Sys_FileClose(FileHandle);
+    //Sys_FileClose(WriteFile);
+    //
+    //free(Buffer);
+    //Buffer = NULL;
 
     // char* ColorData = LoadFile("palette.lmp");
 
