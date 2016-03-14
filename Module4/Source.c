274e712982cc266eb105b149e4277bfc8007d3e7
Philip Buuck <philipbuuck@gmail.com>
2016-03-13 23:40:31 -0400

    Handmade Quake 4.4
    Game Directories and File Loading

--- Module4/Source.c
+++ Module4/Source.c
@@ -5,6 +5,7 @@
 
 #include "file.h"
 
+#pragma pack(push,1)
 typedef struct
 {
     char Magic[4];
@@ -21,7 +22,7 @@ typedef struct
 
 typedef struct  
 {
-    char FileName[64];
+    char FileName[56];
     int  FilePosition;
     int  FileLength;
 } packfile_t;
@@ -33,6 +34,13 @@ typedef struct
     int  NumberOfFiles;
     packfile_t *PakFiles;
 } pack_t;
+
+typedef struct searchpath_s
+{
+    pack_t  *Pack;
+    struct searchpath_s *Next;
+} searchpath_t;
+#pragma pack(pop)
 
 char* va(char* format, ...)
 {
@@ -57,7 +65,7 @@ pack_t *COM_LoadPackFile(char *PakFile)
 
     if (PakHandle == -1)
     {
-        int a = 5; a;
+        return NULL;
     }
 
     dpackheader_t PakHeader;
@@ -66,7 +74,7 @@ pack_t *COM_LoadPackFile(char *PakFile)
 
     if (PakHeader.Magic[0] != 'P' || PakHeader.Magic[1] != 'A' || PakHeader.Magic[2] != 'C' || PakHeader.Magic[3] != 'K')
     {
-        int a = 5; a;
+        return NULL;
     }
 
     int NumPackFiles = PakHeader.DirectoryLength / sizeof(dpackfile_t);
@@ -95,83 +103,65 @@ pack_t *COM_LoadPackFile(char *PakFile)
     return Pak0;
 }
 
+searchpath_t *COM_Searchpaths = NULL;
+
+void COM_AddGameDirectory(const char *Directory)
+{
+    char Buffer[128];
+    pack_t *PakPointer;
+    
+
+    for (int i = 0; ; i++)
+    {
+        sprintf_s(Buffer, 128, "%s\\pak%d.pak", Directory, i);
+        PakPointer = COM_LoadPackFile(Buffer);
+        if (!PakPointer)
+            break;
+        searchpath_t *NewPath = (searchpath_t *)malloc(sizeof(searchpath_t));
+        NewPath->Pack = PakPointer;
+        NewPath->Next = COM_Searchpaths;
+        COM_Searchpaths = NewPath;
+    }
+}
+
+char *COM_FindFile(const char *Filename, int* FileLength)
+{
+    if (!Filename)
+        return NULL;
+
+    searchpath_t *Jogger;
+
+    for (Jogger = COM_Searchpaths; Jogger != NULL; Jogger = Jogger->Next)
+    {
+        pack_t *CurrentPak = Jogger->Pack;
+
+        for (int i = 0; i < CurrentPak->NumberOfFiles; i++)
+        {
+            if (!strcmp(Filename, CurrentPak->PakFiles[i].FileName))
+            {
+                Sys_FileSeek(CurrentPak->PackHandle, CurrentPak->PakFiles[i].FilePosition);
+                
+                char *FileData = (char *)malloc(CurrentPak->PakFiles[i].FileLength);
+                Sys_FileRead(CurrentPak->PackHandle, FileData, CurrentPak->PakFiles[i].FileLength);
+
+                if (FileLength)
+                    *FileLength = CurrentPak->PakFiles[i].FileLength;
+                return (char*)FileData;
+            }
+        }
+    }
+
+    return NULL;
+}
+
 int main()
 {
-    pack_t *Pak0 = COM_LoadPackFile("id1\\pak0.pak");
-    pack_t *Pak1 = COM_LoadPackFile("id1\\pak1.pak");
+    COM_AddGameDirectory("id1");
+
+    int Length = 0;
+    char *PaletteBits = COM_FindFile("gfx/palette.lmp", &Length);
 
     int a = 5; a;
-
-
-    //int PakSize;
-    //int PakHandle = Sys_FileOpenRead("id1\\pak0.pak", &PakSize);
-    //
-    //if (PakHandle == -1)
-    //{
-    //    int a = 5; a;
-    //}
-    //
-    //dpackheader_t PakHeader;
-    //
-    //Sys_FileRead(PakHandle, &PakHeader, sizeof(PakHeader));
-    //
-    //if (PakHeader.Magic[0] != 'P' || PakHeader.Magic[1] != 'A' || PakHeader.Magic[2] != 'C' || PakHeader.Magic[3] != 'K')
-    //{
-    //    int a = 5; a;
-    //}
-    //
-    //int NumPackFiles = PakHeader.DirectoryLength / sizeof(dpackfile_t);
-    //
-    //packfile_t* FilesInMemory = malloc(NumPackFiles * sizeof(packfile_t));
-    //
-    //Sys_FileSeek(PakHandle, PakHeader.DirectoryOffset);
-    //
-    //dpackfile_t TempPackFile[MAX_FILES_IN_PACK];
-    //Sys_FileRead(PakHandle, (void *)TempPackFile, PakHeader.DirectoryLength);
-    //
-    //for (int i = 0; i < NumPackFiles; i++)
-    //{
-    //    strcpy_s(FilesInMemory[i].FileName, 56, TempPackFile[i].FileName);
-    //    FilesInMemory[i].FileLength = TempPackFile[i].FileLength;
-    //    FilesInMemory[i].FilePosition = TempPackFile[i].FilePosition;
-    //}
-    //
-    //pack_t  *Pak0 = (void*)malloc(sizeof(pack_t));
-    //strcpy_s(Pak0->PackName, 64, "id1\\pak0.pak");
-    //Pak0->PackHandle = PakHandle;
-    //Pak0->NumberOfFiles = NumPackFiles;
-    //Pak0->PakFiles = FilesInMemory;
-
-
-
-    //int Size;
-    //int FileHandle = Sys_FileOpenRead("Source.c", &Size);
-    //
-    //if (FileHandle == -1)
-    //{
-    //    int a = 5; a;
-    //}
-    //
-    //void *Buffer = malloc(Size);
-    //
-    //Sys_FileRead(FileHandle, Buffer, Size);
-    //
-    //int WriteFile = Sys_FileOpenWrite("Source.out");
-    //
-    //if (WriteFile == -1)
-    //{
-    //    int a = 5; a;
-    //}
-    //
-    //Sys_FileWrite(WriteFile, Buffer, Size);
-    //
-    //Sys_FileClose(FileHandle);
-    //Sys_FileClose(WriteFile);
-    //
-    //free(Buffer);
-    //Buffer = NULL;
-
-    // char* ColorData = LoadFile("palette.lmp");
 
     return 0;
 }
