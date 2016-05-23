c2bb22abd9803aa09786e8a43044172b98dec41b
Julia Schwarz <julenka@cs.cmu.edu>
2016-05-22 17:21:23 -0700

    Module 3.2 done

---
+++ Module3/sys_win.c
@@ -0,0 +1,151 @@
+#include <Windows.h>
+
+#define BUFFER_WIDTH 640
+#define BUFFER_HEIGHT 480
+#define BYTES_PER_PIXEL 4
+
+#define FULL_SCREEN 0 
+
+int g_running = 1;
+void* g_backBuffer;
+
+BITMAPINFO g_bitmapInfo = { 0 };
+
+
+LRESULT CALLBACK MainWndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+    LRESULT result = 0;
+    switch (uMsg)
+    {
+    case WM_DESTROY:
+    case WM_KEYUP:
+        g_running = 0;
+        break;
+    default:
+        result = DefWindowProc(hWnd, uMsg, wParam, lParam);
+        break;
+    }
+    return result;
+}
+
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
+{
+    // Register window class
+    WNDCLASS wc = { 0 };
+
+    wc.lpfnWndProc = MainWndProc;
+    wc.hInstance = hInstance;
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wc.lpszClassName = "Module 3";
+
+    if (!RegisterClassA(&wc))
+        exit(EXIT_FAILURE);
+
+    HWND mainWindow;
+    DWORD dwStyle = WS_OVERLAPPEDWINDOW;
+    DWORD dwExStyle = 0;
+
+    int fullScreen = FULL_SCREEN;
+    if (fullScreen)
+    {
+        DEVMODE dmScreenSettings = { 0 };
+        dmScreenSettings.dmSize = sizeof(dmScreenSettings);
+        dmScreenSettings.dmPelsWidth = BUFFER_WIDTH;
+        dmScreenSettings.dmPelsHeight = BUFFER_HEIGHT;
+        dmScreenSettings.dmBitsPerPel = 32;
+        dmScreenSettings.dmFields = DM_BITSPERPEL | DM_PELSWIDTH | DM_PELSHEIGHT;
+
+        if (ChangeDisplaySettings(&dmScreenSettings, CDS_FULLSCREEN) != DISP_CHANGE_SUCCESSFUL)
+        {
+            dwExStyle = WS_EX_APPWINDOW;
+            dwStyle = WS_POPUP;
+        }
+        else 
+        {
+            fullScreen = 0;
+        }
+    }
+
+    // Create Window
+    RECT r;
+    r.top = r.left = 0;
+    r.right = BUFFER_WIDTH;
+    r.bottom = BUFFER_HEIGHT;
+
+    AdjustWindowRect(&r, dwStyle, FALSE);
+
+    mainWindow = CreateWindowEx(
+        dwExStyle,
+        "Module 3",
+        "Lesson 3.2",
+        dwStyle,
+        CWMO_DEFAULT,
+        CWMO_DEFAULT,
+        r.right - r.left,
+        r.bottom - r.top,
+        NULL,
+        NULL,
+        hInstance,
+        0);
+
+    if (fullScreen)
+    {
+        SetWindowLong(mainWindow, GWL_STYLE, 0);
+    }
+
+    ShowWindow(mainWindow, SW_SHOWDEFAULT);
+
+
+    // Create bitmapinfo
+    g_bitmapInfo.bmiHeader.biSize = sizeof(g_bitmapInfo.bmiHeader);
+    g_bitmapInfo.bmiHeader.biWidth = BUFFER_WIDTH;
+    g_bitmapInfo.bmiHeader.biHeight = BUFFER_HEIGHT;
+    g_bitmapInfo.bmiHeader.biPlanes = 1;
+    g_bitmapInfo.bmiHeader.biBitCount = 32;
+    g_bitmapInfo.bmiHeader.biCompression = BI_RGB;
+
+    g_backBuffer = malloc(BUFFER_WIDTH * BUFFER_HEIGHT * BYTES_PER_PIXEL);
+
+
+    // Game loop
+    MSG msg;
+    while (g_running)
+    {
+        while (PeekMessage(
+            &msg,
+            NULL,
+            0,
+            0,
+            PM_REMOVE
+        ))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+
+            // set to solid color
+            int* idx = g_backBuffer;
+            for (size_t i = 0; i < BUFFER_WIDTH * BUFFER_HEIGHT; i++)
+            {
+                char r = rand() % 256;
+                char g = rand() % 256;
+                char b = rand() % 256;
+                *idx++ = (r << 16 | g << 8 | b);
+            }
+
+            HDC dc = GetDC(mainWindow);
+            StretchDIBits(
+                dc,
+                0, 0, BUFFER_WIDTH, BUFFER_HEIGHT,
+                0, 0, BUFFER_WIDTH, BUFFER_HEIGHT,
+                g_backBuffer,
+                &g_bitmapInfo,
+                DIB_RGB_COLORS,
+                SRCCOPY
+            );
+            DeleteDC(dc);
+        }
+    }
+
+    return EXIT_SUCCESS;
+}
